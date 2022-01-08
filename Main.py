3d8c8af53ec3628b21aba224e0511f1523d22558
Sigomezgi <sigomezgi@unal.edu.co>
2022-01-08 15:25:13 -0500

    'first'

---
+++ Main.py
@@ -0,0 +1,194 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan  4 17:00:00 2022
+
+@author: usuario
+"""
+
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+import plotly.graph_objects as go
+import datetime as dte
+import calendar
+import numpy as np
+import joblib
+from PIL import Image
+from streamlit_player import st_player
+
+
+image = Image.open('runt.PNG')
+
+header = st.container()
+Dataset = st.container()
+Features = st.container()
+Prediction = st.container()
+Resultado  = st.container()
+
+st.markdown(
+    '''
+    <style>
+    .main{
+        background-color: #F5F5F5;
+    }
+    </style>    
+    ''',
+    unsafe_allow_html=True
+)
+background_color = '#F5F5F5'
+
+st.markdown("""
+<style>
+.big-font {
+    font-size:100px !important;
+    align : Center;
+    color: #00aae4;
+}
+</style>
+""", unsafe_allow_html=True)
+
+def get_data(finename):
+    txi_data = pd.read_csv(finename, parse_dates = ['Fecha'])
+    return txi_data
+
+
+
+with header:
+    st.title('RUNTapp')
+    st_player('https://www.youtube.com/watch?v=MPb90009yWk')
+    
+with Dataset:
+    url = 'https://www.runt.com.co/'
+    st.header('RUNT: Registro Único Nacional de [Tránsito](%s)'%url)
+    
+    Df0 = pd.read_csv('registros_autos.csv')
+    
+    Df0['Registros'] = Df0['Unidades']
+    Df0 = Df0[['Fecha','Registros']]
+    Df0 = Df0.head(60)
+    
+    fig1 = go.Figure(data = go.Table(
+        header = dict(values = list(Df0[['Fecha','Registros']].columns),
+                      fill_color = '#00aae4',#FD8E72
+                      align = 'center',
+                      font=dict( color = '#000000',size=22)),
+        cells = dict(values = [Df0.Fecha, Df0.Registros],
+                     fill_color = '#E5ECF6',
+                     align = 'center',
+                     font=dict( color = '#0a0a0a',size=14 ))))
+    
+    fig1.update_layout(margin = dict(l = 5,r = 5,b= 10,t=10),
+                       paper_bgcolor = background_color)
+    st.write(fig1)
+    
+    Df = get_data('registros_autos.csv')
+    Df['Año']= pd.DatetimeIndex(Df['Fecha']).year
+    Df['Mes']= pd.DatetimeIndex(Df['Fecha']).month
+    
+    #namemonth = {1 : 'Ene', 2:'Feb',3:'Mar', 4:'Abr',5:'May',6:'Jun',7:'Jul',8:'Ago',9:'Sep',10:'Oct',11:'Nov',12:'Dic'}
+    
+    #Df['NamMes'] = Df['Mes'].apply(lambda x: namemonth[x])
+    
+    df = Df.groupby(['Mes','Año'])['Unidades'].sum().unstack()
+    
+    fig = px.line(df)
+    
+    fig.update_layout(
+    
+    yaxis=dict(
+        title_text="Registros",
+        titlefont=dict(size=25),
+        tickfont = dict(size=15)
+    ),
+    xaxis=dict(
+        title_text="Mes",
+        titlefont=dict(size=25),
+        tickfont = dict(size=15)
+    )
+    )
+    
+    fig.update_xaxes(showline=True, linewidth=2, linecolor='black')
+    fig.update_yaxes(showline=True, linewidth=2, linecolor='black')
+    
+    st.write(fig)
+    
+with Prediction:
+    
+    
+    st.header('Predicción')
+    sel_col, col3 = st.columns(2)
+    
+    
+    sel_col.info('¿Para que período de tiempo desea hacer la predicción?')
+    intervalperiod = sel_col.selectbox( 'Período',options=['Día', 'Semana', 'Mes'], index=0)
+    
+    Fecha_inicial = sel_col.date_input('Inicio')
+
+    if (intervalperiod != 'Día'):
+        Fecha_final = sel_col.date_input('Final')
+    else:
+        Fecha_final = Fecha_inicial
+
+    if(intervalperiod != 'Día' and Fecha_final < Fecha_inicial):
+
+        sel_col.error('Revisa las fechas ingresadas')
+        
+    Df_festivos = pd.read_csv('festivos.csv',parse_dates = ['Fecha'])
+    
+    z = Fecha_inicial
+    k = []
+    while z <= Fecha_final:
+        k.append(z)
+        z = z + dte.timedelta(days=1)
+    
+    pred = pd.DataFrame(k, columns = ['Fecha'])
+    
+    pred['Festivo'] = pred['Fecha'].apply(lambda x : 1 if x in Df_festivos['Fecha'].unique() else 0)
+    pred['Year']= pd.DatetimeIndex(pred['Fecha']).year
+    pred['Month']= pd.DatetimeIndex(pred['Fecha']).month
+    pred['Day']= pd.DatetimeIndex(pred['Fecha']).day
+    pred['Dayweek'] = pred['Fecha'].apply(lambda x : calendar.day_name[x.weekday()])
+    
+    namemonth = {1 : 'Jan', 2:'Feb',3:'Mar', 4:'Apr',5:'May',6:'Jun',7:'Jul',8:'Aug',9:'Sep',10:'Oct',11:'Nov',12:'Dec'}
+    Vacaciones = [1,6,7,11,12]
+    pred['NamMonth'] = pred['Month'].apply(lambda x: namemonth[x])
+    pred['Holidays'] = pred['Month'].apply(lambda x: 1 if x in Vacaciones else 0)
+    
+    pred = pred[['Year','Month','Day','Dayweek','Festivo','Holidays']]
+    
+    num = ['Year', 'Month','Day', 'Festivo','Holidays','Dayweek_Friday','Dayweek_Monday','Dayweek_Saturday',
+           'Dayweek_Sunday','Dayweek_Thursday','Dayweek_Tuesday','Dayweek_Wednesday']
+           
+           
+    pred = pd.get_dummies(pred)
+    
+    boton = sel_col.button('Predecir')
+with Resultado:
+    
+    if boton:
+        
+        num2 =  list(pred.columns.values)
+        dim = (pred.shape[0],1)
+        for i in num:
+            if i not in num2:
+                pred[i] = np.zeros(dim)
+        
+        pred = pred[num]
+        
+        modelo = joblib.load('modelo_runt_entrenado.pkl')
+        
+        yrunt = modelo.predict(pred)
+        st.balloons()
+        yrunt = round(sum(yrunt))
+        
+        
+        
+        st.markdown('<p class="big-font"> %s Registros !!!</p>'%yrunt, unsafe_allow_html=True)
+        #col3.markdown('<p class="big-font">Registros !!</p>', unsafe_allow_html=True)
+        st.image(image)
+        
+    
+        
+        
+        
+        
